547e6ac9409928131ac0b35332a19336a7243882
andrey-homenko <homandrej@gmail.com>
2017-09-28 00:42:54 +0300

    Experimental request to REST server JSONPlaceholder

--- src/model.js
+++ src/model.js
@@ -1,6 +1,11 @@
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
 class Model{
     constructor(state = []) {
         this.state = state;
+
+        this.getDataBtn = document.getElementById('getDataBtn');
+        this.getDataBtn.addEventListener('click', this.getData.bind(this));
     };
 
     getItem(id) {
@@ -25,6 +30,36 @@ class Model{
         if(index > -1) {
             this.state.splice(index, 1);
         }
+    }
+
+    httpRequest(url) {
+        return new Promise( function(resolve, reject) {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+
+            xhr.onload = function() {
+                if (this.status === 200) {
+                    resolve(this.response);
+                } else {
+                    const error = new Error(this.statusText);
+                    error.code = this.status;
+                    reject(error);
+                }
+            };
+
+            xhr.onerror = function() {
+                reject(new Error("Network Error"));
+            };
+
+            xhr.send();
+        });
+
+    }
+
+    getData() {
+        this.httpRequest(baseUrl + '/posts').then(response => {
+            console.log(response);
+        });
     }
 }
 
